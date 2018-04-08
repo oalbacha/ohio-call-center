4af1357a64c657d7ab3a6479fd8aa17b1cc99faf
Omar Albacha <oalbacha@gmail.com>
2018-04-08 18:38:10 +0400

    ohio-call-center scaffold

---
+++ db/migrate/20180408092718_create_patients.rb
@@ -0,0 +1,12 @@
+class CreatePatients < ActiveRecord::Migration[5.2]
+  def change
+    create_table :patients do |t|
+      t.string :name
+      t.string :phone
+      t.string :email
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#---
+# Excerpted from "Agile Web Development with Rails 5",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails5 for more book information.
+#---
+# encoding: utf-8
+Patient.delete_all
+Patient.create!(name: 'John Snow',
+                phone: '+97155555555',
+                email: 'example@example.com',    
+                address: '123 main street Dubai UAE 119991')
+# . . .
+Patient.create!(name: 'Catelyn Stark',
+                phone: '+97155544444',
+                email: 'example2@example.com',    
+                address: '123 Second street Dubai UAE 119991')
+# . . .
+Patient.create!(name: 'Sansa Stark',
+                phone: '+97155566666',
+                email: 'example3@example.com',    
+                address: '123 Third street Dubai UAE 119991')
+# . . .
+Patient.create!(name: 'Cersei Baratheon',
+                phone: '+97155577777',
+                email: 'example4@example.com',    
+                address: '123 Fourth street Dubai UAE 119991')
+
