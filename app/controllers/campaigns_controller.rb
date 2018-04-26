c06694959ea812f6eedf94f3298783208cf5ba2f
Omar Albacha <oalbacha@gmail.com>
2018-04-26 16:21:45 +0400

    fixed an issue with relationships of users campaigns

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -69,6 +69,6 @@ class CampaignsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def campaign_params
-      params.require(:campaign).permit(:title, :script, :status, users_ids: [])
+      params.require(:campaign).permit(:title, :script, :status, user_ids: [])
     end
 end
